3a2c4c7cfbf0b56df8d85a11c3efec170b229e22
uplsh580 <uplsh580@github.com>
2020-06-29 14:37:17 +0900

    assn3

---
+++ net_workdir/assn3/checksum_test.py
@@ -0,0 +1,32 @@
+from socket import *
+import os
+import sys
+import struct
+import time
+import select
+import binascii  
+
+ICMP_ECHO_REQUEST = 8
+
+def checksum(string): 
+	csum = 0
+	countTo = (len(string) // 2) * 2  
+	count = 0
+
+	while count < countTo:
+		thisVal = ord(string[count+1]) * 256 + ord(string[count]) 
+		csum = csum + thisVal 
+		csum = csum & 0xffffffff  
+		count = count + 2
+	
+	if countTo < len(string):
+		csum = csum + ord(string[len(string) - 1])
+		csum = csum & 0xffffffff 
+	
+	csum = (csum >> 16) + (csum & 0xffff)
+	csum = csum + (csum >> 16)
+	answer = ~csum 
+	answer = answer & 0xffff 
+	answer = answer >> 8 | (answer << 8 & 0xff00)
+	return answer 
+	
